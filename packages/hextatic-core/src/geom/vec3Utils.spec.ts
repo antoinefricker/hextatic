0e48bb18f5bb9bb516a57b84589147f7950d5932
Antoine Fricker <antoine@marmelab.com>
2024-08-29 02:26:55 +0200

    ci: check gha launch

--- packages/hextatic-core/src/geom/vec3Utils.spec.ts
+++ packages/hextatic-core/src/geom/vec3Utils.spec.ts
@@ -3,7 +3,7 @@ import { describe, expect, it } from 'vitest';
 describe('vec3Utils', () => {
     describe('fromObj', () => {
         it('should convert a simple object to Vec2 type', () => {
-            expect(true).toBe(false);
+            expect(true).toBe(3);
         });
     });
 });
