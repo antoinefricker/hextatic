1804ae884e0969bb085c89fe2837096699f87025
Antoine Fricker <antoine@marmelab.com>
2024-08-29 02:35:45 +0200

    ci: validate mock test

--- packages/hextatic-core/src/geom/vec3Utils.spec.ts
+++ packages/hextatic-core/src/geom/vec3Utils.spec.ts
@@ -3,7 +3,7 @@ import { describe, expect, it } from 'vitest';
 describe('vec3Utils', () => {
     describe('fromObj', () => {
         it('should convert a simple object to Vec2 type', () => {
-            expect(true).toBe(3);
+            expect(true).toBe(true);
         });
     });
 });
