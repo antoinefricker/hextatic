16ebdec63e2535f349362d84f6957e5f84368321
Antoine Fricker <antoine@marmelab.com>
2024-08-29 10:21:46 +0200

    ci: remove mock tests

--- packages/hextatic-core/src/geom/vec3Utils.spec.ts
+++
@@ -1,9 +0,0 @@
-import { describe, expect, it } from 'vitest';
-
-describe('vec3Utils', () => {
-    describe('fromObj', () => {
-        it('should convert a simple object to Vec2 type', () => {
-            expect(true).toBe(true);
-        });
-    });
-});
--- packages/hextatic-core/src/geom/vec3Utils.ts
+++
@@ -1,5 +0,0 @@
-import { type Vec3, type Vec3Object } from '../types';
-
-export const fromObj = ({ x, y, z }: Vec3Object): Vec3 => [x, y, z];
-
-export const clone = ([x, y, z]: Vec3): Vec3 => [x, y, z];
