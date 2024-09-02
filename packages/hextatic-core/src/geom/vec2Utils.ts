30d93b54dff877d0f7f75345b56f17f56a917b38
Antoine Fricker <antoine@marmelab.com>
2024-09-03 00:21:11 +0200

    feat: add vec3 math utilities

--- packages/hextatic-core/src/geom/vec2Utils.ts
+++ packages/hextatic-core/src/geom/vec2Utils.ts
@@ -6,6 +6,8 @@ export const clone = ([x, y]: Vec2): Vec2 => [x, y];
 
 export const scale = ([x, y]: Vec2, s: number): Vec2 => [x * s, y * s];
 
+export const normalize = (v: Vec2): Vec2 => scale(v, 1 / getLength(v));
+
 export const getLength = ([x, y]: Vec2): number => Math.hypot(x, y);
 
 export const setLength = ([x, y]: Vec2, len: number): Vec2 => scale([x, y], len / Math.hypot(x, y));
@@ -17,3 +19,6 @@ export const add = ([x1, y1]: Vec2, [x2, y2]: Vec2): Vec2 => [x1 + x2, y1 + y2];
 export const minus = ([x1, y1]: Vec2, [x2, y2]: Vec2): Vec2 => [x1 - x2, y1 - y2];
 
 export const distance = ([x1, y1]: Vec2, [x2, y2]: Vec2): number => Math.hypot(x1 - x2, y1 - y2);
+
+/** computes the angle in radians with respect to the positive x-axis */
+export const getAngle = ([x, y]: Vec2): number => Math.atan2(-y, -x) + Math.PI;
