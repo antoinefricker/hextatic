5badbc17a5185d73f3f4283ea50337372cd3cde3
Antoine Fricker <antoine@marmelab.com>
2024-09-04 01:29:56 +0200

    chore: ugly exports

--- packages/hextatic-core/src/index.ts
+++ packages/hextatic-core/src/index.ts
@@ -1,1 +1,6 @@
-export const version = '0.1.0';
+export * from './board/rectangularGenerator';
+export * as coords from './geom/coordinates';
+export * as hex from './geom/hexUtils';
+export * as vec2 from './geom/vec2Utils';
+export * as vec3 from './geom/vec3Utils';
+export * from './types';
