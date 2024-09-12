719b95d38cd2c916ee33e32d2bbe9fbc705c7bf7
Antoine Fricker <antoine@marmelab.com>
2024-09-13 00:51:28 +0200

    feat: add reset origin demo

--- packages/hextatic-2d/src/board/Board2D.ts
+++ packages/hextatic-2d/src/board/Board2D.ts
@@ -1,18 +1,22 @@
-import { generateRectangularBoard } from '@antoinefricker/hextatic-core';
+import { coords, generateRectangularBoard, type Vec2, vec2, type Vec3, vec3 } from '@antoinefricker/hextatic-core';
 import { type Board } from '@antoinefricker/hextatic-core';
 import { getBoardSize } from '@antoinefricker/hextatic-core/src/geom/boardUtils';
+import { getHexSize, getHexSpacing } from '@antoinefricker/hextatic-core/src/geom/hexUtils';
 import { Container, Graphics } from 'pixi.js';
 
 import { Button } from './Button';
 import { DemoStrategy } from './demoStrategy/DemoStrategy';
+import { ResetOrigin } from './demoStrategy/ResetOrigin';
 import { ShowAxisDemo } from './demoStrategy/ShowAxis';
 import { ShowDistanceDemo } from './demoStrategy/ShowDistanceDemo';
 import { Tile2D } from './Tile2D';
 
 export class Board2D extends Container {
     private map: Container;
-    private board: Board;
+
+    private _model: Board;
     private _cells: Tile2D[];
+    private _origin: Vec3 = [0, 0, 0];
 
     private demo: DemoStrategy;
 
@@ -20,9 +24,9 @@ export class Board2D extends Container {
         super();
 
         this.demo = new DemoStrategy();
-        this.setDemoMode('distance');
+        this.setDemoMode('resetOrigin');
 
-        this.board = generateRectangularBoard(28, 11, 40);
+        this._model = generateRectangularBoard(28, 11, 40);
 
         this.map = new Container();
         this.map.position.set(100, 100);
@@ -36,19 +40,46 @@ export class Board2D extends Container {
         showDistanceButton.position.set(10, 40);
         this.addChild(showDistanceButton);
 
+        const resetOriginButton = new Button(160, 'Reset origin', () => this.setDemoMode('resetOrigin'));
+        resetOriginButton.position.set(10, 70);
+        this.addChild(resetOriginButton);
+
         this.redrawCells();
+    }
+
+    private _getTilePosition(cubicCoords: Vec3): Vec2 {
+        const hexSize = getHexSize(this._model.radius);
+        const hexSpacing = getHexSpacing(this._model.radius);
+
+        const d2Coords = coords.cubicToGrid(vec3.add(cubicCoords, this.origin));
+        const position = vec2.multiply(d2Coords, [hexSpacing[0], 0.5 * hexSpacing[1]]);
+        const offset = vec2.scale(hexSize, 0.5);
+        return vec2.add(position, offset);
+    }
+
+    public get model(): Board {
+        return this._model;
     }
 
     public get cells(): Tile2D[] {
         return this._cells;
     }
 
+    public get origin(): Vec3 {
+        return this._origin;
+    }
+    public set origin(v: Vec3) {
+        this._origin = vec3.clone(v);
+    }
+
     public redrawCells(): void {
         this.map.removeChildren();
 
         this._cells = [];
-        this.board.tiles.forEach((cubic, index) => {
-            const tile = new Tile2D(cubic, this.board.radius, index);
+        this._model.tiles.forEach((cubic) => {
+            const [x, y] = this._getTilePosition(cubic);
+            const tile = new Tile2D(cubic, this._model.radius);
+            tile.position.set(x, y);
             tile.interactive = true;
             tile.on('mouseover', () => this.demo.onmouseover(cubic));
             tile.on('mouseout', () => this.demo.onmouseout());
@@ -57,7 +88,7 @@ export class Board2D extends Container {
             this._cells.push(tile);
         });
 
-        const [boardWidth, boardHeight] = getBoardSize(this.board);
+        const [boardWidth, boardHeight] = getBoardSize(this._model);
         const debug = new Graphics();
         debug.rect(0, 0, boardWidth, boardHeight);
         debug.stroke({ color: 0xff0000, width: 3 });
@@ -65,10 +96,12 @@ export class Board2D extends Container {
         this.map.addChild(debug);
     }
 
-    private setDemoMode(mode: 'axis' | 'distance'): void {
+    private setDemoMode(mode: 'axis' | 'distance' | 'resetOrigin'): void {
         switch (mode) {
             case 'distance':
                 return this.demo.setStrategy(new ShowDistanceDemo(this));
+            case 'resetOrigin':
+                return this.demo.setStrategy(new ResetOrigin(this));
             default:
                 return this.demo.setStrategy(new ShowAxisDemo(this));
         }
